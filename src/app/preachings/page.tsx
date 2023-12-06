b3c43dc01ac6f314dda1e1f2e75a02e2c91461d0
kikeai <lg5967133@gmail.com>
2023-12-06 12:26:14 -0400

    solucionado el bug de preachings

--- src/app/preachings/page.tsx
+++ src/app/preachings/page.tsx
@@ -1,14 +1,22 @@
 'use client'
-import { useParams } from 'next/navigation'
+import { useEffect } from 'react'
+import { useParams, useRouter } from 'next/navigation'
 import Cards from '../ui/preachings/Cards'
 import Pagination from '../ui/preachings/Pagination'
 import { preachings2 } from '../lib/preachingInfo'
 
 export default function Preachings () {
+  const router = useRouter()
   const params = useParams()
   const paramPage = params.page
   const cardXPage = 6
   const maxPage = Math.ceil(preachings2.length / cardXPage)
+  useEffect(() => {
+    if(typeof paramPage !== 'object' && parseInt(paramPage) > maxPage) {
+      router.push(`/preachings/${maxPage}`)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   const page = paramPage === undefined || paramPage === '0' || typeof paramPage === 'object' ? 1 : parseInt(paramPage)
   const preachings = preachings2.slice((page - 1) * cardXPage, page * cardXPage)
   return (
