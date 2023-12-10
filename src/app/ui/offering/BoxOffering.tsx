1484f2754df5113e9cbf9676c62325a0439df87b
kikeai <lg5967133@gmail.com>
2023-12-10 13:15:14 -0400

    agregado pagomovil method a offering

--- src/app/ui/offering/BoxOffering.tsx
+++ src/app/ui/offering/BoxOffering.tsx
@@ -1,13 +1,15 @@
+import PagomovilMethod from './methods/PagomovilMethod'
 import PayPalMethod from './methods/PayPalMethod'
 
 const BoxOffering = () => {
   return (
-    <div className="w-full pb-10 lg:pb-24 max-w-[1440px]">
-      <div className="flex flex-col items-center w-full p-7 lg:p-16 border-2 gap-8 border-blueI rounded-2xl">
+    <section id='offering' className="w-full pb-10 lg:pb-24 max-w-[1440px]">
+      <div className="flex flex-col items-center w-full p-7 lg:p-16 border-2 gap-16 border-blueI rounded-2xl">
         <h2 className='text-2xl text-center sm:text-4xl text-blueI font-black'>Metodos de pago</h2>
         <PayPalMethod />
+        <PagomovilMethod />
       </div>
-    </div>
+    </section>
   )
 }
 
---
+++ src/app/ui/offering/methods/PagomovilMethod.tsx
@@ -0,0 +1,29 @@
+import BankIcon from "@/app/components/componentSVG/BankIcon";
+
+const PagomovilMethod = () => {
+  return (
+    <article className="flex flex-col w-full gap-8">
+      <header>
+        <h3 className='font-noto font-bold text-xl sm:text-2xl text-blueI'>PagoMovil (Banco de Vzla)</h3>
+        <p className='font-noto text-sm sm:text-base text-blueI'>Datos de Pago</p>
+      </header>
+      <div className="flex flex-col sm:flex-row w-full justify-center items-center sm:justify-between bg-[#d6d6d6] rounded-xl p-6 gap-8">
+        <figure>
+          <BankIcon />
+        </figure>
+
+        <div className="flex w-full flex-col gap-4">
+          <h3 className="font-black text-xl sm:text-2xl text-center sm:text-start text-blueI">Datos del receptor</h3>
+          <div className="">
+            <p className="font-medium text-sm sm:text-base text-blueI">Cédula de Identidad:<br className="sm:hidden" /> <b>11.428.564</b></p>
+            <p className="font-medium text-sm sm:text-base text-blueI">Número de Teléfono:<br className="sm:hidden" /> <b>0424-5495680</b></p>
+            <p className="font-medium text-sm sm:text-base text-blueI">Código de Banco:<br className="sm:hidden" /> <b>0102</b> (Banco de Venezuela)</p>
+          </div>
+          <p className="font-medium break-words text-xs sm:text-sm text-blueI">Recuerda que al realizar tu diezmo u ofrenda mediante pago movil puedes reportarlo através de nuestro correo electronico: <b>libertadencristojesus.report@gmail.com</b>, si no lo tomaremos como Donación anónima</p>
+        </div>
+      </div>
+    </article>
+  );
+}
+ 
+export default PagomovilMethod;
