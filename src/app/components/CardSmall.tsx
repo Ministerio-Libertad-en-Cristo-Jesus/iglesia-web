6cf95b7ac478b561e08fca7538821052bc7f48e8
kikeai <lg5967133@gmail.com>
2024-01-26 19:00:40 -0400

    cambiado pastor por author

--- src/app/components/CardSmall.tsx
+++ src/app/components/CardSmall.tsx
@@ -4,7 +4,7 @@ import { type PreachType } from '../lib/definitions'
 import { dateSeter } from '../lib/dateSeter'
 import Image from 'next/image'
 
-const CardSmall = ({ title, pastor, content, date, image, id }: PreachType) => {
+const CardSmall = ({ title, author, content, date, image }: PreachType) => {
   const router = useRouter()
   return (
     <article
@@ -18,7 +18,7 @@ const CardSmall = ({ title, pastor, content, date, image, id }: PreachType) => {
       alt={title} />
       <div className='m-4'>
         <h2 className='  font-bold text-blueI text-base '>{title}</h2>
-        <p className='font noto text-blueI mt-1 font-medium text-xs '>{pastor}</p>
+        <p className='font noto text-blueI mt-1 font-medium text-xs '>{author}</p>
         <p className='  text-blueI mt-2 font-medium text-xs line-clamp-2'>{content[0]}</p>
         <p className='  text-gray-600 mt-3 text-xs'>{dateSeter(date)}</p>
       </div>
