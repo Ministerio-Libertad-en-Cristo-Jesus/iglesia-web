958e89a774f047e7a9cf898dddac9b7b00084047
kikeai <lg5967133@gmail.com>
2024-01-27 21:11:16 -0400

    solucionado bug de leer mas de carrousel en movil

--- src/app/ui/home/carrouselSection/Slide.tsx
+++ src/app/ui/home/carrouselSection/Slide.tsx
@@ -8,7 +8,7 @@ const Slide = ({ title, content, image }: PreachType) => {
 
       <div className='w-full h-[600px] absolute top-0 right-0 max-w-[1440px] bg-gradient-to-t from-black'></div>
 
-      <div className='absolute left-10 bottom-8 lg:left-24 lg:bottom-16'>
+      <div className='absolute left-10 bottom-12 lg:left-24 lg:bottom-16'>
         <h2 className='font-black text-whiteI max-w-4xl text-xl mr-10 lg:mr-0 lg:text-4xl'>{title}</h2>
         <p className='font-extralight line-clamp-4 text-whiteI text-xs lg:text-sm max-w-[850px] mt-1 mr-10 lg:mr-0 lg:mt-5'>{content}</p>
         <Link href={`/news/${title}`} className='font-bold text-whiteI text-sm lg:text-base mt-4 mb-10 lg:mt-5 underline w-max hover:text-blue-700 hover:cursor-pointer transition-all duration-300'>Leer más</Link>
