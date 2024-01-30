50e9777cbcb42299df6411f98ae4e860c459e247
kikeai <lg5967133@gmail.com>
2024-01-30 17:59:33 -0400

    arreglado bug inicio

--- src/app/ui/home/carrouselSection/Slide.tsx
+++ src/app/ui/home/carrouselSection/Slide.tsx
@@ -10,7 +10,7 @@ const Slide = ({ title, content, image }: PreachType) => {
 
       <div className='absolute left-10 bottom-12 lg:left-24 lg:bottom-16'>
         <h2 className='font-black text-whiteI max-w-4xl text-xl mr-10 lg:mr-0 lg:text-4xl'>{title}</h2>
-        <p className='font-extralight line-clamp-4 text-whiteI text-xs lg:text-sm max-w-[850px] mt-1 mr-10 lg:mr-0 lg:mt-5'>{content}</p>
+        <p className='font-extralight line-clamp-4 text-whiteI text-xs lg:text-sm max-w-[850px] mt-1 mr-10 lg:mr-0 lg:mt-5 mb-4'>{content}</p>
         <Link href={`/news/${title}`} className='font-bold text-whiteI text-sm lg:text-base mt-4 mb-10 lg:mt-5 underline w-max hover:text-blue-700 hover:cursor-pointer transition-all duration-300'>Leer más</Link>
       </div>
 
